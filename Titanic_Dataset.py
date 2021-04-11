940bb69afa3e09f18037387759572e8522235525
davinakeane1 <davinakeane1@gmail.com>
2021-04-11 20:05:04 +0100

    Fourth commit - Survival bar chart

--- Titanic_Dataset.py
+++ Titanic_Dataset.py
@@ -19,3 +19,13 @@ titanic_df.isnull().sum()
 #See survived (1) vs not survived (0)
 survived_figure = titanic_df.groupby('Survived')['Survived'].count()
 print(survived_figure)
+
+#bar chart to see survived vs not survived
+plt.figure()
+plt.style.use('seaborn-darkgrid')
+plt.bar(survived_figure.index, survived_figure.values, color='lavender',  edgecolor='purple')
+plt.title('Survived vs Did Not survive')
+plt.xticks([0,1],['Did Not survive', 'Survived'])
+for i, value in enumerate(survived_figure.values):
+    plt.text(i, value-60, str(value), color='black', fontsize=10, style='oblique', horizontalalignment='center')
+plt.show()
