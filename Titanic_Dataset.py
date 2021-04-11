143cabb5c8d54faf14d4e069d2ae40ca35c90237
davinakeane1 <davinakeane1@gmail.com>
2021-04-11 17:25:23 +0100

    second commit

--- Titanic_Dataset.py
+++ Titanic_Dataset.py
@@ -13,4 +13,5 @@ os.chdir(r"C:\Users\College\Desktop\DataAnalytics")
 #loading in the csv file
 titanic_df = pd.read_csv("tested.csv", encoding = "ISO-8859-1")
 
-print(titanic_df)
+print(titanic_df.isnull().sum())
+
